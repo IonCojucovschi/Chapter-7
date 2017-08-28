7e1cfd9c23b728cfbcc2ba2fb51c3dc4adbe0fb5
IonCojucovschi <ion.cojucovschi@outlook.com>
2017-08-29 01:20:29 +0300

    Home First Commmit

---
+++ SportsSolution/SportsStore/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Entities;
+
+namespace SportsStore.Concrete
+{
+    class EFDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SportsSolution/SportsStore/Concrete/EFProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Abstract;
+using SportsStore.Entities;
+
+namespace SportsStore.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+
+        }
+    }
+}
